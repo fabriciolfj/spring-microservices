8404683c22bb2c9345bf85bf6c8b2bfe3c53a7b6
fabricio <fabricio.jacob@outlook.com>
2020-05-18 08:58:46 -0300

    add

--- mssc-ssm/src/main/java/guru/springframework/msscssm/domain/Payment.java
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -1,4 +1,20 @@
 package guru.springframework.msscssm.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState state;
+
+    private BigDecimal amount;
 }
--- mssc-ssm/src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
+++ mssc-ssm/src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -1,4 +1,5 @@
 package guru.springframework.msscssm.domain;
 
-public class PaymentEvent {
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINED
 }
